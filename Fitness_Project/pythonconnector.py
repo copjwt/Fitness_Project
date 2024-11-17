a9af1cf5ff95395418afaeaad824868bfede5a8f
copjwt <jenwit15.t@gmail.com>
2024-11-18 00:55:53 +0700

    new file:   Fitness_Project/index.html 	new file:   Fitness_Project/mairoo..py 	modified:   Fitness_Project/pythonconnector.py 	new file:   Fitness_Project/script.js 	new file:   Fitness_Project/signin.html 	new file:   Fitness_Project/style.css

--- Fitness_Project/pythonconnector.py
+++ Fitness_Project/pythonconnector.py
@@ -4,7 +4,7 @@ import re
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Copter482625#"
+    password="Copter482625#",
     database = "fitness"
 )
 
@@ -12,7 +12,9 @@ mycursor = db.cursor()
 mycursor.execute("CRATE TABLE User (UserID int(8) PRIMARY KEY AUTO INCREMENT)")
 #mycursor.execute("INSERT INTO Person (name,age) VALUES (%s,%s)",("Cop",20))
 #mycursor.execute("CREATE TABLE Test (name VARCHAR(50) NOT NULL, created datetime NOT NULL, gender ENUM('M','F','O')NOT NULL, ID int PRIMARY KEY NOT NULL AUTO_INCREMENT)")
-#db.commit()
+
+
+db.commit()
 
 # mycursor.execute("SELECT * FROM Person")
 # for x in mycursor:
